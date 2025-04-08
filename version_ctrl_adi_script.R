eccc5a6d2db5dd8c7924253477fc3ecdb561e47f
AditiDR1 <aditiramanandapsb@gmail.com>
2025-04-08 10:42:07 -0400

    wip

--- version_ctrl_adi_script.R
+++ version_ctrl_adi_script.R
@@ -2,4 +2,7 @@ install.packages("palmerpenguins")
 
 penguins <- palmerpenguins::penguins
 
-penguins
+penguins %>% 
+  ggplot() +
+  geom_point(aes(x = body_mass_g,
+                y = flipper_length_mm))
