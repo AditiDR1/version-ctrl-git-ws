f971dc4f3d874add2133a673366dd809484faac1
AditiDR1 <aditiramanandapsb@gmail.com>
2025-04-08 10:42:31 -0400

    wip 2

--- version_ctrl_adi_script.R
+++ version_ctrl_adi_script.R
@@ -5,4 +5,7 @@ penguins <- palmerpenguins::penguins
 penguins %>% 
   ggplot() +
   geom_point(aes(x = body_mass_g,
-                y = flipper_length_mm))
+                y = flipper_length_mm,
+                color = island)) +
+  theme_minimal()
+
